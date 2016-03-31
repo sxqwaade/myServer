b8a8b8cf03a739a656010b82c5ab832c03c0fb0c
shawn <sxqwade@sina.com>
2016-03-31 16:43:09 +0800

    updates

---
+++ api/spider.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var path = require('path'),
+    fs = require('fs');
+
+exports.searchSomething = function(){
+
+};
--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,11 @@
 
 var index = require('./index');
 var upload = require('../api/upload');
+var spider = require('../api/spider');
 var multiparty = require('connect-multiparty'),
     multipartyMiddleware = multiparty();
 module.exports = function (app) {
     app.get('/excel2json', index.excel2json);
+    app.get('/api/spider/searchSomething',spider.searchSomething);
     app.post('/api/excel2json',multipartyMiddleware,upload.excel2json);
 };
